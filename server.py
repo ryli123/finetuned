f08da2dd46efe43454f0d555d9d7e2c4c30e2f62
Someone-1243 <justinhye@gmail.com>
2021-01-16 00:32:45 -0500

    basic upload page

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+
+
+ALLOWED_EXTENSIONS = {'jpg','png','jpeg'}
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            print("file_received")
+        
+        return render_template("index.html")
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
