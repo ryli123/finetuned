92884f32d170a713acdf593ba5a4421fa8d74841
Ryan Li <li.ryan.go@gmail.com>
2021-01-17 00:52:23 -0500

    hotfix for genius api

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 Session(app)
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
