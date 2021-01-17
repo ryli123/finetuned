01b7908b939a0f13caef0939fe4e51a6542204ef
Tiger Ye <bobcorpisepic@gmail.com>
2021-01-16 23:46:27 -0500

    pls fix

--- phototest.py
+++ phototest.py
@@ -31,17 +31,20 @@ def song_to_image(image, song_extract, sentiment): #pass sentiment in too
     # danceability, tempo, energy, valence, mode
 
     # STEP 1: saturation
-    sat_ratio1 = 0.70
+    sat_ratio1 = 0.75
     sat_ratio2 = 0.75
+    sat_ratio3 = 0.5
     
-    saturation = song_extract[3]*sat_ratio1 + song_extract[0]*sat_ratio2 
+    saturation = song_extract[3]*sat_ratio1 + song_extract[0]*sat_ratio2 - song_extract[6]*sat_ratio3 
     print(saturation)
     photo = ImageEnhance.Color(photo).enhance(saturation)
 
     # STEP 2: contrast
-    con_ratio1 = 2
+    con_ratio1 = 1.2
+    con_ratio2 = 0.8
     
-    contrast = song_extract[2]*con_ratio1
+    contrast = song_extract[2]*con_ratio1 + -song_extract[5]/60*con_ratio2
+    print(contrast)
     photo = ImageEnhance.Contrast(photo).enhance(contrast)
 
     #STEP 3: brightness
@@ -56,7 +59,7 @@ def song_to_image(image, song_extract, sentiment): #pass sentiment in too
     sha_ratio2 = 1
     sha_ratio3 = 2
     
-    sharpness = song_extract[0]*sha_ratio1 + song_extract[4]*sha_ratio2 + song_extract[2]*sha_ratio3
+    sharpness = song_extract[0]*sha_ratio1 + song_extract[4]*sha_ratio2 + -song_extract[5]/60*sha_ratio3
     photo = ImageEnhance.Sharpness(photo).enhance(sharpness)
 
     #invert colors idk if we can use this
@@ -79,14 +82,14 @@ def song_to_image(image, song_extract, sentiment): #pass sentiment in too
 
     this feels kinda over kill
     '''
-
+    '''
     if (song_extract[4] == 0):
         pic = photo.filter(ImageFilter.SMOOTH)
         pic.save("ImageFilter_MinFilter_9.jpg")
     if (song_extract[4] == 1):
         pic = photo.filter(ImageFilter.SMOOTH) #<--- idk
         pic.save("ImageFilter_MaxFilter_9.jpg")
-
+    '''
     
     #SENTIMENT ANALYSIS, to fix any previous adjustments 
 
